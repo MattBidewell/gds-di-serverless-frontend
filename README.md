5ca88253643017cfcb557d8764f485014f805696
Matthew Bidewell <matthewbidewell@gmail.com>
2023-01-03 21:10:38 +0000

    add dynamoDB table

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ There are two pages, one input a text value and one to display that.
 
 ## Version Two
 
+### TODO
+
 Hooks the above to a dynamo DB with increase permissions. Here we generate a session and store it in dynamo db where also store session data and return that to the user. Purpose here is to test any issues with a simulated full journey.
 
 ## Version Three
--- lambdas/versionTwo/app.js
+++ lambdas/versionTwo/app.js
@@ -1,11 +1,27 @@
 const { setup } = require("hmpo-app");
+const AWS = require("aws-sdk");
 const HmpoFormWizard = require("hmpo-form-wizard");
 const serverlessExpress = require('@vendia/serverless-express')
+
+const session = require("express-session");
+const DynamoDBStore = require("connect-dynamodb")(session);
+
+const SESSION_TABLE_NAME = process.env.SESSION_TABLE_NAME
+
+AWS.config.update({
+  region: "eu-west-2",
+});
+const dynamodb = new AWS.DynamoDB();
+const dynamoDBSessionStore = new DynamoDBStore({
+  client: dynamodb,
+  table: SESSION_TABLE_NAME,
+});
 
 const sessionConfig = {
   cookieName: "service_session",
   cookieOptions: { maxAge: 7200000 },
   secret: "MySuperSecret",
+  ...(SESSION_TABLE_NAME && { sessionStore: dynamoDBSessionStore }),
 };
 
 const PORT = 3000;
